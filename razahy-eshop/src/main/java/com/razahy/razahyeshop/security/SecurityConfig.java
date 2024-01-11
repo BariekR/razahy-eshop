434493b8fe8325ef2d9ebbafaa7fa2212dfc1a69
Ramez Bariekzahy <ramez@seznam.cz>
2024-01-11 12:32:05 +0100

    Security configured for InMemoryUserDetailsManager

--- razahy-eshop/src/main/java/com/razahy/razahyeshop/security/SecurityConfig.java
+++ razahy-eshop/src/main/java/com/razahy/razahyeshop/security/SecurityConfig.java
@@ -2,17 +2,43 @@ package com.razahy.razahyeshop.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(r -> {
-                    r.anyRequest().permitAll();
-                })
+                .authorizeHttpRequests(r -> {r.anyRequest().authenticated();})
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        UserDetails user = User.withUsername("Razahy").password(encoder.encode("1234")).authorities("user").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
 }
