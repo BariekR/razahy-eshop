33f4b6638fe15b8d5b63095104648dece22deded
Ramez Bariekzahy <ramez@seznam.cz>
2024-01-15 11:15:36 +0100

    login page styling and error message added

--- razahy-eshop/src/main/java/com/razahy/razahyeshop/controller/AccountController.java
+++ razahy-eshop/src/main/java/com/razahy/razahyeshop/controller/AccountController.java
@@ -1,12 +1,20 @@
 package com.razahy.razahyeshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AccountController {
+    private static final String LOGIN_ERROR = "Bad credentials";
+
     @GetMapping("/login")
-    public String getLogin() {
+    public String getLogin(Model model, Optional<String> error) {
+        if (error.isPresent()) {
+            model.addAttribute("error", LOGIN_ERROR);
+        }
         return "login";
     }
 }
